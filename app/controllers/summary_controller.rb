31479f19f2874f033f1d820c473af9a1ae1ba28f
Andrew Cookson <acookson91@gmail.com>
2016-06-03 09:44:24 +0100

    summary page now shows total events, test passing

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -1,2 +1,8 @@
 class SummaryController < ApplicationController
+
+  def index
+    @email = Email.all
+    @total_events = @email.count
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'summary#index'
+  get 'summary/index' => 'email#index'
   post 'webhooks/receive' => 'webhooks#receive'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
