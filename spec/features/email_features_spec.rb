2d9656861461f1d4b1fa4435b8ee2ddfbfdb22d5
Andrew Cookson <acookson91@gmail.com>
2016-06-03 09:46:42 +0100

    added failing test for summary of opened emails

--- spec/features/email_features_spec.rb
+++ spec/features/email_features_spec.rb
@@ -5,11 +5,15 @@ context 'summary page' do
 
   before do
     create_email_data
+    visit '/'
   end
 
-  scenario 'number of email events recieved' do
-    visit '/'
+  scenario 'shows number of email events recieved' do
     expect(page).to have_content('Total Email Events: 4')
+  end
+
+  scenario 'shows number of opened emails' do
+    expect(page).to have_content('Opened: 1')
   end
 
 end
