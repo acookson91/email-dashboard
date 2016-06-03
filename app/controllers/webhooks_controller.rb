fb3fd32956dc616cfdb22ebd79fe920e3756a37a
Andrew Cookson <acookson91@gmail.com>
2016-06-03 13:37:32 +0100

    webhood intergration set up now but unit test for controller not completed

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,11 @@
 class WebhooksController < ApplicationController
 
+  skip_before_filter  :verify_authenticity_token
+
+  def receive
+    email = JSON.parse( request.body.read )
+    Email.create(Address: email['Address'],EmailType: email['EmailType'],Event: email['Event'], Timestamp: email['Timestamp'])
+    render :nothing => true
+  end
 
 end
