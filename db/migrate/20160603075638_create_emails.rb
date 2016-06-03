91ed5d043300917f9d0b09e892234c9a88a5663c
Andrew Cookson <acookson91@gmail.com>
2016-06-03 09:29:59 +0100

    added db seed to populate email db, now to set up summary controller

--- db/migrate/20160603075638_create_emails.rb
+++ db/migrate/20160603075638_create_emails.rb
@@ -1,6 +1,10 @@
 class CreateEmails < ActiveRecord::Migration
   def change
     create_table :emails do |t|
+      t.string :Address
+      t.string :EmailType
+      t.string :Event
+      t.integer :Timestamp
 
       t.timestamps null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+email1 = Email.create(:Address => 'barney@lostmy.name', :EmailType => 'Shipment',
+     :Event => 'send', :Timestamp => 1432820696)
+
+email2 = Email.create(:Address => 'tom@lostmy.name', :EmailType => 'UserConfirmation',
+          :Event => 'click', :Timestamp => 1432820702)
+
+email3 = Email.create(:Address => 'vitor@lostmy.name', :EmailType => 'Shipment',
+                     :Event => 'open', :Timestamp => 1432820702)
